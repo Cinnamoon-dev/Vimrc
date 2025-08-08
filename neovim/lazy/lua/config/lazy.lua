c6551807d1c309800ac6db6d33d0b545f8a860ef
Cinnamoon-dev <pedrolcsilva10@gmail.com>
2025-08-08 18:53:25 -0300

    refactor: configuring autoclose inside plugin file

--- neovim/lazy/lua/config/lazy.lua
+++ neovim/lazy/lua/config/lazy.lua
@@ -17,28 +17,3 @@ require("lazy").setup("plugins")
 vim.g.gruvbox_bold = '0'
 vim.g.gruvbox_italic = '0'
 
-require("autoclose").setup({
-    keys = {
-	["("] = { escape = false, close = true, pair = "()" },
-	["["] = { escape = false, close = true, pair = "[]" },
-	["{"] = { escape = false, close = true, pair = "{}" },
-
-	[">"] = { escape = true, close = false, pair = "<>" },
-	[")"] = { escape = true, close = false, pair = "()" },
-	["]"] = { escape = true, close = false, pair = "[]" },
-	["}"] = { escape = true, close = false, pair = "{}" },
-
-	['"'] = { escape = true, close = true, pair = '""' },
-	["'"] = { escape = true, close = true, pair = "''" },
-	["`"] = { escape = true, close = true, pair = "``" },
-    },
-    options = {
-	disabled_filetypes = { "text" },
-	disable_when_touch = false,
-	touch_regex = "[%w(%[{]",
-	pair_spaces = false,
-	auto_indent = true,
-	disable_command_mode = false,    
-    },
-})
-
